2c465ae694e68615416d196d604ac129a2ea1557
EmmanuelUdoh <emmanuelubongudoh2002@gmail.com>
2024-06-12 22:50:39 +0100

    first

--- src/DappToDo_frontend/src/App.jsx
+++ src/DappToDo_frontend/src/App.jsx
@@ -1,29 +1,26 @@
 import { useState } from 'react';
 import { DappToDo_backend } from 'declarations/DappToDo_backend';
+import './App.css';
+import { TodoWrapper } from './components/TodoWrapper';
+
 
 function App() {
-  const [greeting, setGreeting] = useState('');
+  // const [greeting, setGreeting] = useState('');
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const name = event.target.elements.name.value;
-    DappToDo_backend.greet(name).then((greeting) => {
-      setGreeting(greeting);
-    });
-    return false;
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const name = event.target.elements.name.value;
+  //   DappToDo_backend.greet(name).then((greeting) => {
+  //     setGreeting(greeting);
+  //   });
+  //   return false;
+  // }
 
   return (
     <main>
-      <img src="/logo2.svg" alt="DFINITY logo" />
-      <br />
-      <br />
-      <form action="#" onSubmit={handleSubmit}>
-        <label htmlFor="name">Enter your name: &nbsp;</label>
-        <input id="name" alt="Name" type="text" />
-        <button type="submit">Click Me!</button>
-      </form>
-      <section id="greeting">{greeting}</section>
+    <div className="App">
+      <TodoWrapper  />
+    </div>
     </main>
   );
 }
